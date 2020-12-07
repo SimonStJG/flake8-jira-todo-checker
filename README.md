46a59d321423fae13bb7091b8d262279179a32de
Simon StJG <Simon.StJG@gmail.com>
2020-12-07 21:49:19 +0000

    Add support for checking state of cards in JIRA and support for allowed and disallowed TODOs

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ Flake8 JIRA TODO Checker
 [![PyPI](https://img.shields.io/pypi/pyversions/flake8-jira-todo-checker.svg)](https://pypi.python.org/pypi/flake8-jira-todo-checker)
 [![PyPI](https://img.shields.io/pypi/format/flake8-jira-todo-checker.svg)](https://pypi.python.org/pypi/flake8-jira-todo-checker)
 
-Flake8 plugin to check that every `TODO`, `FIXME`, `QQ` etc comment has a JIRA ID next to it.
+Flake8 plugin to check that:
 
-In other words, this is valid:
+ 1. Every `TODO`, `FIXME`, `QQ` etc comment has a JIRA ID next to it.  
+ 2. Every JIRA ID refers to a JIRA card which is not closed.
+
+In other words, this is valid as long as the JIRA card ABC-123 is not closed:
 
 ```
 def hacky_function():
@@ -40,14 +43,70 @@ If no project IDs are provided then all TODOs will be rejected.
 
 ### todo-synonyms
 
-A list of words which will be treated like TODO can be provided via the flag `--todo-synonyms` or via the key 
-`todo-synonyms` in a flake8 configuration file.  Defaults to:
+A list of words which will be treated like TODO can be provided via the flags `--allowed-todo-synonyms` and 
+`--disallowed-todo-synonyms` or via the key `allowed-todo-synonyms` and `disallowed-todo-synonyms` in a flake8 
+configuration file.  
+
+`disallowed-todo-synonyms` will raise an error whenever found in the codebase, and `allowed-todo-synonyms` will raise an 
+error only if it's missing a JIRA card or that JIRA card is invalid.
+
+Defaults to:
 ```
-todo-synonyms = TODO,FIX,QQ
+allowed-todo-synonyms = TODO
+disallowed-todo-synonyms = FIX,QQ
 ```
+
+### jira-server
+
+The URL of the JIRA server, if unset the status of JIRA cards won't be checked.
+
+
+### disallowed-jira-statuses, disallowed-jira-resolutions, and disallow-all-jira-resolutions
+
+If a TODO is attached to a JIRA issue whose status is in `disallowed-jira-statuses` then an error will be reported, 
+ditto if the JIRA card has a resolution in `disallowed-jira-resolutions`.  If `disallow-all-jira-resolutions` is set to 
+`True`, then report an error if issue has any resolution. 
+
+Defaults to:
+```
+disallowed-jira-statuses = Done
+disallow-all-jira-resolutions = True
+```
+
+### JIRA Authentication
+
+We support the same authentication methods as the 
+[jira-python](https://jira.readthedocs.io/en/master/examples.html#authentication) library.
+
+For cookie-based username/password authentication, use the following configuration parameters:
+
+1.  `jira-cookie-username`
+1.  `jira-cookie-password`
+
+For HTTP Basic username/password authentication, use the following configuration parameters:
+
+1.  `jira-http-basic-username`
+1.  `jira-http-basic-password`
+
+For OAuth authentication, use the following configuration parameters:
+
+1.  `jira-oauth-access-token`
+1.  `jira-oauth-access-token-secret`
+1.  `jira-oauth-consumer-key`
+1.  `jira-oauth-key-cert-file`
+
+For kerberos authentication, set the `jira-kerberos` configuration parameter to True. 
+
+## Alternatives
+
+This project is heavily inspired by the [Softwire TODO checker](https://github.com/Softwire/todo-checker).
 
 ## Releasing
 
 1. `poetry run bump2version minor`
 1. `git push && git push --tags`
 1. `tox -e pypi`
+
+# TODO
+
+Test and add instructions for JIRA cloud.
---
+++ flake8_jira_todo_checker/jira_client.py
@@ -0,0 +1,139 @@
+import logging
+from pathlib import Path
+
+import jira
+
+logger = logging.getLogger(__name__)
+
+
+class JiraClient:
+    def __init__(self, jira_client):
+        self._jira_client = jira_client
+
+    def get_issue(self, issue_id):
+        try:
+            issue = self._jira_client.issue(issue_id, fields=["status", "resolution"])
+        except jira.JIRAError as e:
+            if e.status_code != 404:
+                raise
+            return None
+
+        status = issue.fields.status.name
+        if issue.fields.resolution:
+            resolution = issue.fields.resolution.name
+        else:
+            resolution = None
+        return status, resolution
+
+
+def add_jira_client_options(parser):
+    parser.add_option(
+        "--jira-server", action="store", parse_from_config=True, help="JIRA Server URL, e.g. http://localhost:8080"
+    )
+
+    parser.add_option(
+        "--jira-cookie-username",
+        action="store",
+        parse_from_config=True,
+        help="JIRA Cookie-based auth: Username",
+        default=None,
+    )
+    parser.add_option(
+        "--jira-cookie-password",
+        action="store",
+        parse_from_config=True,
+        help="JIRA Cookie-based auth: Password",
+        default=None,
+    )
+    parser.add_option(
+        "--jira-http-basic-username",
+        action="store",
+        parse_from_config=True,
+        help="JIRA HTTP Basic Auth: Username",
+        default=None,
+    )
+    parser.add_option(
+        "--jira-http-basic-password",
+        action="store",
+        parse_from_config=True,
+        help="JIRA HTTP Basic Auth: Password",
+        default=None,
+    )
+    parser.add_option(
+        "--jira-oauth-access-token", action="store", parse_from_config=True, help="JIRA OAuth: Access Token"
+    )
+    parser.add_option(
+        "--jira-oauth-access-token-secret",
+        action="store",
+        parse_from_config=True,
+        help="JIRA OAuth: Access Token Secret",
+    )
+    parser.add_option(
+        "--jira-oauth-consumer-key", action="store", parse_from_config=True, help="JIRA OAuth: Consumer Key"
+    )
+    parser.add_option(
+        "--jira-oauth-key-cert-file", action="store", parse_from_config=True, help="JIRA OAuth: Key Cert File"
+    )
+    parser.add_option("--jira-kerberos", action="store_true", parse_from_config=True, help="JIRA Kerberos Auth")
+
+
+def jira_client_from_options(options):
+    kwargs = {}
+
+    jira_server = options.jira_server
+
+    if not jira_server:
+        logger.debug("Not using JIRA client")
+        return None
+
+    kwargs["server"] = jira_server
+
+    jira_cookie_username = options.jira_cookie_username
+    jira_cookie_password = options.jira_cookie_password
+    jira_http_basic_username = options.jira_http_basic_username
+    jira_http_basic_password = options.jira_http_basic_password
+    jira_oauth_access_token = options.jira_oauth_access_token
+    jira_oauth_access_token_secret = options.jira_oauth_access_token_secret
+    jira_oauth_consumer_key = options.jira_oauth_consumer_key
+    jira_oauth_key_cert_file = options.jira_oauth_key_cert_file
+    jira_kerberos = options.jira_kerberos
+
+    is_cookie_auth = any([jira_cookie_username, jira_cookie_password])
+    is_basic_auth = any([jira_http_basic_username, jira_http_basic_password])
+    is_oauth = any(
+        [jira_oauth_access_token, jira_oauth_access_token_secret, jira_oauth_consumer_key, jira_oauth_key_cert_file]
+    )
+    is_kerberos = jira_kerberos
+
+    auth_methods_requested = {
+        "Cookie Auth": is_cookie_auth,
+        "Basic Auth": is_basic_auth,
+        "OAuth": is_oauth,
+        "Kerberos": is_kerberos,
+    }
+
+    num_auth_methods_requested = sum(auth_methods_requested.values())
+    if num_auth_methods_requested == 0:
+        raise ValueError("No JIRA authentication details provided")
+    elif num_auth_methods_requested > 1:
+        names_of_auth_methods_requested = [name for name, was_requested in auth_methods_requested if was_requested]
+        raise ValueError(
+            f"Too many JIRA authentication methods requested: {', '.join(names_of_auth_methods_requested)}"
+        )
+    elif is_cookie_auth:
+        kwargs["auth"] = (jira_cookie_username, jira_cookie_password)
+    elif is_basic_auth:
+        kwargs["basic_auth"] = (jira_http_basic_username, jira_http_basic_password)
+    elif is_oauth:
+        kwargs["oauth"] = {
+            "access_token": jira_oauth_access_token,
+            "access_token_secret": jira_oauth_access_token_secret,
+            "consumer_key": jira_oauth_consumer_key,
+            "key_cert": Path(jira_oauth_key_cert_file).read_text(),
+        }
+    elif is_kerberos:
+        kwargs["kerberos"] = True
+    else:
+        raise RuntimeError("Programmer error - unhandled case")
+
+    return JiraClient(jira.JIRA(**kwargs))
